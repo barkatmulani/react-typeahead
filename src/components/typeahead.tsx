a638796d8391b0794bbd95c19f5e67be7bd5cf6e
Barkat Mulani <barkatmulani@yahoo.com>
2024-08-24 11:45:32 +1000

    Bootstrap package installed and basic list.

--- src/components/typeahead.tsx
+++ src/components/typeahead.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { ListGroup } from "react-bootstrap";
+
 type Props = {
     list: string[];
     debounceTime?: number; // In milliseconds
@@ -5,14 +8,28 @@ type Props = {
 }
 
 export default function Typeahead({ list, debounceTime = 1000, onChange}: Props) {
+    const [ items, setItems ] = useState<string[]>([]);
+
     const onInputChange = (e: any) => {
-        console.log(e.target.value);
+        const filtered = list.filter(x => x.includes(e.target.value));
+        setItems(filtered);
         onChange && onChange(e.target.value);
+    };
+
+    const onItemClicked = (e: any) => {
+        console.log(e.target.innerText);
     };
 
     return (
         <div>
             <input title="Input" type="text" onChange={onInputChange} />
+            <ListGroup>
+                {items && items.map((item: string, i: number) =>
+                    <ListGroup.Item key={i} onClick={onItemClicked}>
+                        {item}
+                    </ListGroup.Item>
+                )}
+            </ListGroup>
         </div>
-    )
+    );
 }
